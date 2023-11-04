182d3a4eb92de13a575b05d5ee2533fd67c429c3
oBraganca <thayllonryan17@gmail.com>
2023-11-04 11:30:49 -0300

    Clean Auth controllers import

--- src/main/java/com/application/controller/AuthController.java
+++ src/main/java/com/application/controller/AuthController.java
@@ -1,18 +1,9 @@
 package com.application.controller;
 
-import org.hibernate.validator.internal.util.stereotypes.Lazy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,18 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.application.dto.Response;
 import com.application.dto.UserDto;
-import com.application.model.Role;
-import com.application.model.User;
-import com.application.model.UserCustomizations;
-import com.application.repository.RoleRepository;
-import com.application.repository.UserCustomizationsRepositoy;
-import com.application.repository.UserRepository;
 import com.application.service.AuthenticationService;
-import com.application.service.TokenService;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
